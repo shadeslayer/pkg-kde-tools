a960265bcd031a45226e6bd14c847d8c4cc92b72
pusling-guest <pusling-guest@fe476991-40d1-0310-a08b-93a1e690bb93>
2008-10-29 17:19:02 +0000

    * The no .svn dir in release release. * Add a way to set --as-needed during build. * there was a reference to ENABLE_FINAL in variables.mk. Kill it.

--- makefiles/1/variables.mk
+++ makefiles/1/variables.mk
@@ -4,7 +4,6 @@ DEB_CONFIG_INSTALL_DIR ?= /usr/share/kde4/config
 # Standard Debian KDE 4 cmake flags
 DEB_CMAKE_KDE4_FLAGS += \
         -DCMAKE_BUILD_TYPE=Debian \
-        -DKDE4_ENABLE_FINAL=$(KDE4-ENABLE-FINAL) \
         -DKDE4_BUILD_TESTS=false \
         -DKDE_DISTRIBUTION_TEXT="Debian packages" \
         -DKDE_DEFAULT_HOME=.kde4 \
@@ -16,3 +15,20 @@ DEB_CMAKE_KDE4_FLAGS += \
         -DKCFG_INSTALL_DIR=/usr/share/kde4/config.kcfg \
         -DLIB_INSTALL_DIR=/usr/lib \
         -DSYSCONF_INSTALL_DIR=/etc
+
+# Set hhe DEB_KDE_LINK_WITH_AS_NEEDED to 'yes' to enable linking
+# with --as-needed (off by default)
+DEB_KDE_LINK_WITH_AS_NEEDED ?= no
+ifneq (,$(findstring yes, $(DEB_KDE_LINK_WITH_AS_NEEDED)))
+    ifeq (,$(findstring no-as-needed, $(DEB_BUILD_OPTIONS)))
+        DEB_KDE_LINK_WITH_AS_NEEDED := yes
+        DEB_CMAKE_CUSTOM_FLAGS += \
+            -DCMAKE_SHARED_LINKER_FLAGS="-Wl,--no-undefined -Wl,--as-needed" \
+            -DCMAKE_MODULE_LINKER_FLAGS="-Wl,--no-undefined -Wl,--as-needed" \
+            -DCMAKE_EXE_LINKER_FLAGS="-Wl,--no-undefined -Wl,--as-needed"
+    else
+        DEB_KDE_LINK_WITH_AS_NEEDED := no
+    endif
+else
+    DEB_KDE_LINK_WITH_AS_NEEDED := no
+endif
