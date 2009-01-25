0eb3370e4e79c6f0e3d372338f7b1e2080d247e7
modax-guest <modax-guest@fe476991-40d1-0310-a08b-93a1e690bb93>
2009-01-25 10:27:12 +0000

    Improve comment

--- qt-kde-team/1/policy.mk
+++ qt-kde-team/1/policy.mk
@@ -6,7 +6,7 @@ include /usr/share/cdbs/1/rules/buildvars.mk
 DEB_KDE_DISTRIBUTION := $(shell dpkg-parsechangelog | grep '^Distribution: ' | sed 's/^Distribution: \(.*\)/\1/g')
 DEB_KDE_MAJOR_VERSION := $(shell echo "$(DEB_UPSTREAM_VERSION)" | cut -d. -f1-2)
 
-# Policy file may not exist. It is fine
+# Distribution-specific policy file may not exist. It is fine
 ifeq (,$(filter $(DEB_KDE_DISTRIBUTION),$(DEB_KDE_DISABLE_POLICY_CHECK)))
   -include $(DEB_PKG_KDE_QT_KDE_TEAM)/policy/$(DEB_KDE_DISTRIBUTION).mk
 endif
