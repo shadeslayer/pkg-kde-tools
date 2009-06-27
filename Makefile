54f1611f71fc2c872193d78386a66a313fbf69eb
Modestas Vainius <modestas@vainius.eu>
2009-06-27 22:20:13 +0300

    Add very basic pkgkde-symbolshelper which supports 1.15.3
    As stated in changelog, it is here only to avoid breaking packages
    using it at build time. It is absolute useless for patching/generating
    new symbol files.
    
    Signed-off-by: Modestas Vainius <modestas@vainius.eu>

--- Makefile
+++ Makefile
@@ -11,7 +11,7 @@ install:
 	# symbolshelper
 	cd $(SYMBOLSHELPER_DIR) && find Debian -type f -name "*.pm" -exec \
 	    install -D -m 0644 {} $(DESTDIR)/$(PERLLIBDIR)/{} \;
-	install -m 0755 $(SYMBOLSHELPER_DIR)/pkgkde-symbolshelper $(DESTDIR)/usr/bin
+	install -m 0755 $(SYMBOLSHELPER_DIR)/pkgkde-symbolshelper-basic $(DESTDIR)/usr/bin/pkgkde-symbolshelper
 	
 	# dh_sameversiondep
 	install -d $(MANDIR)/man1
--- makefiles/1/cdbs/symbolshelper.mk
+++ makefiles/1/cdbs/symbolshelper.mk
@@ -10,9 +10,6 @@ ifneq (,$(strip $(SYMBOLSHELPER_PACKAGES)))
 $(patsubst %,binary-strip/%,$(SYMBOLSHELPER_PACKAGES)):: binary-strip/%:
 	pkgkde-symbolshelper symbolfile -p $(cdbs_curpkg) -o debian/$(cdbs_curpkg).symbols.$(DEB_HOST_ARCH)
 
-$(patsubst %,binary-fixup/%,$(SYMBOLSHELPER_PACKAGES)):: binary-fixup/%:
-	pkgkde-symbolshelper postgensymbols -p $(cdbs_curpkg) -v
-
 clean::
 	rm -f $(patsubst %,debian/%.symbols.$(DEB_HOST_ARCH),$(SYMBOLSHELPER_PACKAGES))
 
