40cff968d799b0d57a12a1192db38d82ea7a71a6
Modestas Vainius <modestas@vainius.eu>
2009-07-02 19:42:22 +0300

    Add a basic debhelper/kde.mk snippet.
    For building KDE packages with dh (needs debhelper >= 7.3.0).
    
    Signed-off-by: Modestas Vainius <modestas@vainius.eu>

---
+++ makefiles/1/debhelper/kde.mk
@@ -0,0 +1,19 @@
+# Include default KDE 4 cmake configuration variables
+include /usr/share/pkg-kde-tools/makefiles/1/variables.mk
+
+# Check if debhelper (>= 7.3) is installed
+DEB_DH_VERSION := $(shell perl -MDebian::Debhelper::Dh_Version -e \
+    'my $$v=$$Debian::Debhelper::Dh_Version::version;\
+     my @v=split(/\./,$$v); \
+     print (($$v[0]>7 || $$v[0]==7 && $$v[1]>=3) ? "ok" : $$v), "\n";' 2>/dev/null)
+ifneq ($(DEB_DH_VERSION),ok)
+    $(error Debhelper is too old ($(DEB_DH_VERSION)) on your system. Upgrade to 7.3.0 or later)
+endif
+
+# Configure with KDE cmake flags by default.
+DEB_KDE_OVERRIDE_DH_AUTO_CONFIGURE ?= override_dh_auto_configure
+$(DEB_KDE_OVERRIDE_DH_AUTO_CONFIGURE):
+	dh_auto_configure -- $(DEB_CMAKE_KDE4_FLAGS) $(DEB_CMAKE_CUSTOM_FLAGS)
+
+%:
+	dh $@
