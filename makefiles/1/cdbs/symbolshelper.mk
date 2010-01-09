7ef232e6fb5806bfce29bb7574e142bc85f828a8
Modestas Vainius <modestas@vainius.eu>
2010-01-09 18:58:36 +0200

    Make cdbs/symbolshelper.mk add /usr/share/pkg-kde-tools/bin to $PATH.

--- makefiles/1/cdbs/symbolshelper.mk
+++ makefiles/1/cdbs/symbolshelper.mk
@@ -3,6 +3,10 @@ include /usr/share/cdbs/1/rules/buildvars.mk
 ifndef _cdbs_pkgkde_symbolshelper
 _cdbs_pkgkde_symbolshelper = 1
 
+ifneq (/usr/share/pkg-kde-tools/bin,$(filter /usr/share/pkg-kde-tools/bin,$(subst :, ,$(PATH))))
+    export PATH := /usr/share/pkg-kde-tools/bin:$(PATH)
+endif
+
 SYMBOLSHELPER_PACKAGES := $(filter $(DEB_PACKAGES),$(patsubst debian/%.symbols.in,%,$(wildcard debian/*.symbols.in)))
 
 ifneq (,$(strip $(SYMBOLSHELPER_PACKAGES)))
