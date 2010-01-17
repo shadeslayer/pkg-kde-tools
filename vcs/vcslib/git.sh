6f0a6613b1df2968d9ae4ef803a2da11630328d8
Modestas Vainius <modestas@vainius.eu>
2010-01-18 01:22:24 +0200

    Update my address in the code copyright notices, update debian/copyright.

--- vcs/vcslib/git.sh
+++ vcs/vcslib/git.sh
@@ -1,4 +1,4 @@
-# Copyright (C) 2009 Modestas Vainius <modestas@vainius.eu>
+# Copyright (C) 2009 Modestas Vainius <modax@debian.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
