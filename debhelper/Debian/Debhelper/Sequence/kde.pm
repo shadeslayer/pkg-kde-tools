2fcb281b795fca186e44d167d519d2f509c7d752
Modestas Vainius <modestas@vainius.eu>
2010-02-13 15:22:20 +0200

    dh kde addon: exclude kde documentation from dh_compress by default.

--- debhelper/Debian/Debhelper/Sequence/kde.pm
+++ debhelper/Debian/Debhelper/Sequence/kde.pm
@@ -35,4 +35,8 @@ add_command_options("dh_auto_clean", "--buildsystem=kde");
 # Omit usr/lib/kde4 from dh_makeshlibs by default
 add_command_options("dh_makeshlibs", "-Xusr/lib/kde4/");
 
+# Exclude kde documentation from dh_compress by default
+add_command_options("dh_compress",
+    qw(-X.dcl -X.docbook -X-license -X.tag -X.sty -X.el));
+
 insert_after("dh_install", "dh_movelibkdeinit");
