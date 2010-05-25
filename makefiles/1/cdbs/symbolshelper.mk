c56d9e4f5704f0245c60cd1103a0884b9cef6c15
Modestas Vainius <modestas@vainius.eu>
2010-05-25 17:32:48 +0300

    Drop deprecated `pkgkde-symbolshelper symbolfile` support from cdbs/symbolshelper.mk.

--- makefiles/1/cdbs/symbolshelper.mk
+++ makefiles/1/cdbs/symbolshelper.mk
@@ -7,16 +7,4 @@ ifneq (/usr/share/pkg-kde-tools/bin,$(filter /usr/share/pkg-kde-tools/bin,$(subs
     export PATH := /usr/share/pkg-kde-tools/bin:$(PATH)
 endif
 
-SYMBOLSHELPER_PACKAGES := $(filter $(DEB_PACKAGES),$(patsubst debian/%.symbols.in,%,$(wildcard debian/*.symbols.in)))
-
-ifneq (,$(strip $(SYMBOLSHELPER_PACKAGES)))
-
-$(patsubst %,binary-strip/%,$(SYMBOLSHELPER_PACKAGES)):: binary-strip/%:
-	pkgkde-symbolshelper symbolfile -p $(cdbs_curpkg) -o debian/$(cdbs_curpkg).symbols.$(DEB_HOST_ARCH)
-
-clean::
-	rm -f $(patsubst %,debian/%.symbols.$(DEB_HOST_ARCH),$(SYMBOLSHELPER_PACKAGES))
-
-endif
-
 endif
