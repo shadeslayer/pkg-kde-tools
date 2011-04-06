aad9b1993b043a308472b691cf60d9df91c939d0
Modestas Vainius <modax@debian.org>
2011-04-06 20:44:03 +0300

    Fix another bug in libpkgs_gen_strict_local_shlibs regex.

--- qt-kde-team/2/library-packages.mk
+++ qt-kde-team/2/library-packages.mk
@@ -42,7 +42,7 @@ libpkgs_gen_strict_local_shlibs:
 	set -e; \
 	if [ -n "`ls debian/*.substvars 2>/dev/null`" ]; then \
 	    echo "Generating strict local shlibs on packages: $(libpkgs_gen_strict_local_shlibs)"; \
-	    sed -i '/^shlibs:[^=]\+=/{ s/\(^shlibs:[^=]\+=[[:space:]]*\|,[[:space:]]*\)\($(libpkgs_re)\)\([[:space:]]*([[:space:]]*>=[^)]\+)\)\?/\1\2 (= $(libpkgs_binver))/g }' debian/*.substvars; \
+	    sed -i '/^shlibs:[^=]\+=/{ s/\(^shlibs:[^=]\+=[[:space:]]*\|,[[:space:]]*\)\($(libpkgs_re)\)\([[:space:]]*([[:space:]]*[><=]\+[^)]\+)\)\?/\1\2 (= $(libpkgs_binver))/g }' debian/*.substvars; \
     fi
 
 $(foreach t,binary-arch binary,post_$(t)_dh_shlibdeps): libpkgs_gen_strict_local_shlibs
