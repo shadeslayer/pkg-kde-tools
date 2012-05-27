db8f05962c34d6e8289014001a5f0ae3b218f100
Modestas Vainius <modax@debian.org>
2012-05-27 15:21:22 +0300

    Implement pkgkde-git clone and update-config subcommands.

--- vcslib/git.sh
+++ vcslib/git.sh
@@ -18,8 +18,9 @@
 
 git_tag()
 {
-    local tag_path tag_msg
+    shift $(opt_till_double_dash "$@")
 
+    local tag_path tag_msg
     is_distribution_valid || die "invalid Debian distribution for tagging - $DEB_DISTRIBUTION"
     git_is_working_tree_clean || die "working tree is dirty. Commit changes before tagging."
 
@@ -27,6 +28,101 @@ git_tag()
     tag_msg="$DEB_VERSION $DEB_DISTRIBUTION; urgency=$DEB_URGENCY"
 
     runcmd git tag $tag_path -m "$tag_msg" "$@"
+}
+
+git_clone()
+{
+    local url pushurl
+    url="git://git.debian.org/pkg-kde"
+    pushurl="git.debian.org:/git/pkg-kde"
+
+    # Parse remaining command line (or -- if any) options
+    local name
+    while getopts ":u:p:" name; do
+        case "$name" in
+            u) url="$OPTARG" ;;
+            p) pushurl="$OPTARG";;
+            ?)  if [ -n "$OPTARG" ]; then OPTIND=$(($OPTIND-1)); fi; break;;
+            :)  die "$OPTARG option is missing a required argument" ;;
+        esac
+    done
+    if [ "$OPTIND" -gt 1 ]; then
+        shift "$(($OPTIND-1))"
+    fi
+
+    local repo dir
+    repo="${1%.git}"
+    dir="`dirname "$repo"`"
+    shift 1 # Shift repo
+
+    shift $(opt_till_double_dash "$@")
+
+    url="${url}/${repo}.git"
+    pushurl="${pushurl}/${repo}.git"
+
+    if [ -d "$repo" ]; then
+        die "$repo repository already exists locally"
+    fi
+    if [ ! -d "$dir" ]; then
+        die "repository parent directory $dir does not exist on the local filesystem. Create it"
+    fi
+
+    info "Cloning $url (pushurl: $pushurl)"
+    runcmd git clone "$@" -- "$url" "$repo"
+
+    info "Updating configuration of the new repository"
+    cd "$repo"
+    runcmd git config remote.origin.pushurl "$pushurl"
+
+    git_update_config
+}
+
+git_update_config()
+{
+    local force
+
+    # Parse remaining command line (or -- if any) options
+    local name
+    while getopts ":f" name; do
+        case "$name" in
+            f) force="y";;
+            ?)  if [ -n "$OPTARG" ]; then OPTIND=$(($OPTIND-1)); fi; break;;
+            :)  die "$OPTARG option is missing a required argument" ;;
+        esac
+    done
+    if [ "$OPTIND" -gt 1 ]; then
+        shift "$(($OPTIND-1))"
+    fi
+
+    if [ -n "$force" ] || ! git config --get-all remote.origin.push "$@" > /dev/null; then
+        info "[ok] Setting up repository to push master and debian tags by default."
+        runcmd git config --replace-all remote.origin.push "refs/heads/master"
+        runcmd git config --add remote.origin.push "refs/tags/debian/*"
+    else
+        info "[skip] Push specs already present."
+    fi
+
+    if [ -n "$DEBFULLNAME" ]; then
+        if [ "$force" = "y" ] || ! git config --file=.git/config --get "user.name" "$@" >/dev/null 2>&1; then
+            info "[ok] Setting user.name to the value of the DEBFULLNAME environment variable: $DEBFULLNAME."
+            runcmd git config user.name "$DEBFULLNAME"
+        else
+            info "[skip] user.name configuration option is already set"
+        fi
+    else
+        info "[skip] DEBFULLNAME environment variable is not set. Not setting user.name."
+    fi
+
+    if [ -n "$DEBEMAIL" ]; then
+        if [ "$force" = "y" ] || ! git config --file=.git/config --get "user.email" "$@" >/dev/null 2>&1; then
+            info "[ok] Setting user.email to the value of the DEBEMAIL environment variable: $DEBEMAIL."
+            runcmd git config user.email "$DEBEMAIL"
+        else
+            info "[skip] user.email configuration option is already set"
+        fi
+    else
+        info "[skip] DEBEMAIL environment variable is not set. Not setting user.email."
+    fi
 }
 
 git_compat_debver()
@@ -39,34 +135,38 @@ git_is_working_tree_clean()
     git update-index --refresh > /dev/null && git diff-index --quiet HEAD
 }
 
-
-PACKAGE_ROOT="$(readlink -f "$(git rev-parse --git-dir)/..")"
-
-# Do some envinronment sanity checks first
-if [ "$(git rev-parse --is-bare-repository)" = "true" ]; then
-    die "bare Git repositories are not supported."
-fi
-
-is_valid_package_root "$PACKAGE_ROOT" || 
-    die "$PACKAGE_ROOT does NOT appear to be a a valid debian packaging repository"
-
 # Get subcommand name
 test "$#" -gt 0  || die "subcommand is NOT specified"
 subcmd="$1"; shift
 
-# Get info about debian package
-get_debian_package_info "$PACKAGE_ROOT"
+subcmd_needs_package_root="1"
+if [ "$subcmd" = "clone" ]; then
+    subcmd_needs_package_root=""
+fi
 
-# Parse remaining command line (or -- if any) options
-while getopts ":" name; do
-    case "$name" in
-        ?)  if [ -n "$OPTARG" ]; then OPTIND=$(($OPTIND-1)); fi; break;;
-        :)  die "$OPTARG option is missing a required argument" ;;
-    esac
-done
+# Do some envinronment sanity checks first
+git_is_bare="$(git rev-parse --is-bare-repository 2>/dev/null)"
+if [ "$?" -eq 0 ]; then
+    if [ -z "$subcmd_needs_package_root" ]; then
+        is_valid_package_root "$PACKAGE_ROOT" ||
+            die "$subcmd should not be executed inside a valid debian packaging repository"
+    fi
 
-if [ "$OPTIND" -gt 1 ]; then
-    shift "$(($OPTIND-1))"
+    if [ "$git_is_bare" = "true" ]; then
+        die "bare Git repositories are not supported."
+    fi
+
+    PACKAGE_ROOT="$(readlink -f "$(git rev-parse --git-dir)/..")"
+
+    is_valid_package_root "$PACKAGE_ROOT" ||
+        die "$PACKAGE_ROOT does NOT appear to be a valid debian packaging repository"
+
+    # Get info about debian package
+    get_debian_package_info "$PACKAGE_ROOT"
+else
+    if [ -n "$subcmd_needs_package_root" ]; then
+        die "$subcmd should be executed inside a valid debian packaging git repository"
+    fi
 fi
 
 # Execute subcommand
@@ -74,8 +174,14 @@ case "$subcmd" in
     tag)
         git_tag "$@"
         ;;
+    clone)
+        git_clone "$@"
+        ;;
+    update_config|update-config)
+        git_update_config "$@"
+        ;;
     *)
-        die "unsupported pkgkde-vcs Git subcommand: $subcmd"
+        die "unsupported pkgkde-vcs Git subcommand: $subcmd. Commands available: clone, tag, update-config"
         ;;
 esac
 
