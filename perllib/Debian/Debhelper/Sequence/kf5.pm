74dcdabcb28919033d3d9b4c5635caa621251687
Rohan Garg <rohan@kde.org>
2014-08-07 18:50:35 +0200

    Add new sequence helpers for KDE Frameowrks 5
    New sequence helpers for KDE Frameworks 5, please use
    dh --with kf5 for apps using KDE Frameworks 5, and
    qt-kde-team/3/debian-qt-kde.mk at the top of packages
    released by KDE.

---
+++ perllib/Debian/Debhelper/Sequence/kf5.pm
@@ -0,0 +1,41 @@
+{
+    package Debian::Debhelper::Sequence::kde;
+    use Debian::Debhelper::Dh_Version;
+    use Debian::Debhelper::Dh_Lib qw(error);
+
+    sub ensure_debhelper_version {
+        my @v = split(/\./, $Debian::Debhelper::Dh_Version::version);
+        if ($v[0] > $_[0]) {
+            return 1;
+        }
+        elsif ($v[0] == $_[0]) {
+            if ($v[1] > $_[1]) {
+                return 1;
+            }
+            elsif ($v[1] == $_[1]) {
+                return $1 >= $_[2] if ($v[2] =~ /^(\d+)/);
+            }
+        }
+        return 0;
+    }
+    unless (ensure_debhelper_version(7, 3, 16)) {
+        error "debhelper addon 'kde' requires debhelper 7.3.16 or later";
+    }
+
+    1;
+}
+
+# Build with kde buildsystem by default
+add_command_options("dh_auto_configure", "--buildsystem=kde");
+add_command_options("dh_auto_build", "--buildsystem=kde");
+add_command_options("dh_auto_test", "--buildsystem=kde");
+add_command_options("dh_auto_install", "--buildsystem=kde");
+add_command_options("dh_auto_clean", "--buildsystem=kde");
+
+# Exclude kde documentation from dh_compress by default
+add_command_options("dh_compress",
+    qw(-X.dcl -X.docbook -X-license -X.tag -X.sty -X.el));
+
+insert_after("dh_install", "dh_movelibkdeinit");
+
+1;
